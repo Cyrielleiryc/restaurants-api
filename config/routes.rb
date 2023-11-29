5684d4574dc90ed3c813135e00bbbf876ea49cfa
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-29 14:15:45 +0100

    endpoints 'index' and 'show'

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: %i[index show]
+    end
+  end
 end
